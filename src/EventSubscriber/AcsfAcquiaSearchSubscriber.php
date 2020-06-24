5045675e57f96dda92153fe96e19ede7893e6f62
Alejandro Garza <alejandro.garza@acquia.com>
2020-06-24 16:12:01 -0400

    Only set hash if different

--- src/EventSubscriber/AcsfAcquiaSearchSubscriber.php
+++ src/EventSubscriber/AcsfAcquiaSearchSubscriber.php
@@ -13,15 +13,20 @@ class AcsfAcquiaSearchSubscriber implements EventSubscriberInterface {
    *  Create a hash that contains the site name, DB name and Site folder.
    */
   public function setSearchApiSiteHash(GetResponseEvent $event) {
+
     $ah_site_name = $_ENV['AH_SITE_NAME']) ?: '';
-    if ($ah_site_name) {
-      $conf_path = \Drupal::service('site.path');
-      $sites_foldername = substr($conf_path, strrpos($conf_path, '/') + 1);
+    if (!$ah_site_name) {
+      return;
+    }
 
-      $options = Database::getConnection()->getConnectionOptions();
-      $ah_db_name = $options['database'];
-      $hash = $ah_site_name . '-' . $ah_db_name . '-' . $sites_foldername;
+    $conf_path = \Drupal::service('site.path');
+    $sites_foldername = substr($conf_path, strrpos($conf_path, '/') + 1);
 
+    $options = Database::getConnection()->getConnectionOptions();
+    $ah_db_name = $options['database'];
+    $hash = $ah_site_name . '-' . $ah_db_name . '-' . $sites_foldername;
+
+    if (\Drupal::state()->get('search_api_solr.site_hash') != $hash) {
       \Drupal::state()->set('search_api_solr.site_hash', $hash);
     }
 
