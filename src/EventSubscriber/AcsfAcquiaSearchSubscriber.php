4f50f884f7b242c6be48158c299f8129bd0dfcd2
Alejandro Garza <alejandro.garza@acquia.com>
2020-12-04 14:14:14 -0500

    Moved to Eventsubscriber-only change

--- src/EventSubscriber/AcsfAcquiaSearchSubscriber.php
+++ src/EventSubscriber/AcsfAcquiaSearchSubscriber.php
@@ -14,7 +14,7 @@ class AcsfAcquiaSearchSubscriber implements EventSubscriberInterface {
    */
   public function setSearchApiSiteHash(GetResponseEvent $event) {
 
-    $ah_site_name = $_ENV['AH_SITE_NAME']) ?: '';
+    $ah_site_name = getenv('AH_SITE_NAME') ?: '';
     if (!$ah_site_name) {
       return;
     }
@@ -22,10 +22,10 @@ class AcsfAcquiaSearchSubscriber implements EventSubscriberInterface {
     $conf_path = \Drupal::service('site.path');
     $sites_foldername = substr($conf_path, strrpos($conf_path, '/') + 1);
 
-    $options = Database::getConnection()->getConnectionOptions();
-    $ah_db_name = $options['database'];
-    $hash = $ah_site_name . '-' . $ah_db_name . '-' . $sites_foldername;
+    $db_options = Database::getConnection()->getConnectionOptions();
+    $ah_db_name = $db_options['database'];
 
+    $hash = "acsf:{$ah_site_name}:{$ah_db_name}:{$sites_foldername}";
     if (\Drupal::state()->get('search_api_solr.site_hash') != $hash) {
       \Drupal::state()->set('search_api_solr.site_hash', $hash);
     }
